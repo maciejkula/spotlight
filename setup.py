b07705edc7adedaa5806a816c4c8bd9a832650bd
maciejkula <maciej.kula@gmail.com>
2017-06-28 22:08:59 +0100

    Remove all install_requires.

--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ from setuptools import setup
 setup(
     name='spotlight',
     version='0.1.0',
-    install_requires=['numpy',
-                      'scipy',
-                      'h5py',
-                      'requests'],
     packages=['spotlight'],
     license='MIT',
     classifiers=['Development Status :: 3 - Alpha',
