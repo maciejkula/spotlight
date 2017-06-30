3ecf443e5266b42deb960b9f850ad90d90096a1e
maciejkula <maciej.kula@gmail.com>
2017-06-30 21:24:03 +0100

    Add user-based split.

---
+++ tests/test_cross_validation.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from spotlight import cross_validation
+from spotlight.datasets import movielens
+
+
+RANDOM_STATE = np.random.RandomState(42)
+
+
+def test_user_based_split():
+
+    interactions = movielens.get_movielens_dataset('100K')
+
+    train, test = (cross_validation
+                   .user_based_train_test_split(interactions,
+                                                test_percentage=0.2,
+                                                random_state=RANDOM_STATE))
+
+    assert len(train) + len(test) == len(interactions)
+
+    users_in_test = len(np.unique(test.user_ids))
+    assert np.allclose(users_in_test / interactions.num_users,
+                       0.2, atol=0.001)
