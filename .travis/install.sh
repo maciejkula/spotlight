e5d888f72a2de138326fef46ff4f759d671d2b2a
maciejkula <maciej.kula@gmail.com>
2017-07-08 15:18:10 +0100

    Get conda quietly.

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,9 +4,9 @@ set -e
 
 # Download and install conda
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
+    wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
 else
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+    wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 fi
 
 bash ~/miniconda.sh -b -p $HOME/miniconda
