2c1a679a844a7e5b631ad57ac0b7ac4b7bfb0778
maciejkula <maciej.kula@gmail.com>
2017-07-27 21:56:54 +0100

    Start hybridising factorization models.

--- spotlight/cross_validation.py
+++ spotlight/cross_validation.py
@@ -6,53 +6,7 @@ import numpy as np
 
 from sklearn.utils import murmurhash3_32
 
-from spotlight.interactions import Interactions
-
-
-def _index_or_none(array, shuffle_index):
-
-    if array is None:
-        return None
-    else:
-        return array[shuffle_index]
-
-
-def shuffle_interactions(interactions,
-                         random_state=None):
-    """
-    Shuffle interactions.
-
-    Parameters
-    ----------
-
-    interactions: :class:`spotlight.interactions.Interactions`
-        The interactions to shuffle.
-    random_state: np.random.RandomState, optional
-        The random state used for the shuffle.
-
-    Returns
-    -------
-
-    interactions: :class:`spotlight.interactions.Interactions`
-        The shuffled interactions.
-    """
-
-    if random_state is None:
-        random_state = np.random.RandomState()
-
-    shuffle_indices = np.arange(len(interactions.user_ids))
-    random_state.shuffle(shuffle_indices)
-
-    return Interactions(interactions.user_ids[shuffle_indices],
-                        interactions.item_ids[shuffle_indices],
-                        ratings=_index_or_none(interactions.ratings,
-                                               shuffle_indices),
-                        timestamps=_index_or_none(interactions.timestamps,
-                                                  shuffle_indices),
-                        weights=_index_or_none(interactions.weights,
-                                               shuffle_indices),
-                        num_users=interactions.num_users,
-                        num_items=interactions.num_items)
+from spotlight.interactions import Interactions, _slice_or_none
 
 
 def random_train_test_split(interactions,
@@ -79,8 +33,7 @@ def random_train_test_split(interactions,
          A tuple of (train data, test data)
     """
 
-    interactions = shuffle_interactions(interactions,
-                                        random_state=random_state)
+    interactions.shuffle(random_state=random_state)
 
     cutoff = int((1.0 - test_percentage) * len(interactions))
 
@@ -89,22 +42,30 @@ def random_train_test_split(interactions,
 
     train = Interactions(interactions.user_ids[train_idx],
                          interactions.item_ids[train_idx],
-                         ratings=_index_or_none(interactions.ratings,
+                         ratings=_slice_or_none(interactions.ratings,
                                                 train_idx),
-                         timestamps=_index_or_none(interactions.timestamps,
+                         timestamps=_slice_or_none(interactions.timestamps,
                                                    train_idx),
-                         weights=_index_or_none(interactions.weights,
+                         weights=_slice_or_none(interactions.weights,
                                                 train_idx),
+                         user_features=interactions.user_features,
+                         context_features=_slice_or_none(interactions.context_features,
+                                                         train_idx),
+                         item_features=interactions.item_features,
                          num_users=interactions.num_users,
                          num_items=interactions.num_items)
     test = Interactions(interactions.user_ids[test_idx],
                         interactions.item_ids[test_idx],
-                        ratings=_index_or_none(interactions.ratings,
+                        ratings=_slice_or_none(interactions.ratings,
                                                test_idx),
-                        timestamps=_index_or_none(interactions.timestamps,
+                        timestamps=_slice_or_none(interactions.timestamps,
                                                   test_idx),
-                        weights=_index_or_none(interactions.weights,
+                        weights=_slice_or_none(interactions.weights,
                                                test_idx),
+                        user_features=interactions.user_features,
+                        context_features=_slice_or_none(interactions.context_features,
+                                                        test_idx),
+                        item_features=interactions.item_features,
                         num_users=interactions.num_users,
                         num_items=interactions.num_items)
 
@@ -154,21 +115,21 @@ def user_based_train_test_split(interactions,
 
     train = Interactions(interactions.user_ids[in_train],
                          interactions.item_ids[in_train],
-                         ratings=_index_or_none(interactions.ratings,
+                         ratings=_slice_or_none(interactions.ratings,
                                                 in_train),
-                         timestamps=_index_or_none(interactions.timestamps,
+                         timestamps=_slice_or_none(interactions.timestamps,
                                                    in_train),
-                         weights=_index_or_none(interactions.weights,
+                         weights=_slice_or_none(interactions.weights,
                                                 in_train),
                          num_users=interactions.num_users,
                          num_items=interactions.num_items)
     test = Interactions(interactions.user_ids[in_test],
                         interactions.item_ids[in_test],
-                        ratings=_index_or_none(interactions.ratings,
+                        ratings=_slice_or_none(interactions.ratings,
                                                in_test),
-                        timestamps=_index_or_none(interactions.timestamps,
+                        timestamps=_slice_or_none(interactions.timestamps,
                                                   in_test),
-                        weights=_index_or_none(interactions.weights,
+                        weights=_slice_or_none(interactions.weights,
                                                in_test),
                         num_users=interactions.num_users,
                         num_items=interactions.num_items)
--- tests/factorization/test_implicit.py
+++ tests/factorization/test_implicit.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 
 from spotlight.cross_validation import random_train_test_split
-from spotlight.datasets import movielens
+from spotlight.datasets import movielens, synthetic
 from spotlight.evaluation import mrr_score
 from spotlight.factorization.implicit import ImplicitFactorizationModel
 
@@ -118,3 +118,30 @@ def test_adaptive_hinge():
     mrr = mrr_score(model, test, train=train).mean()
 
     assert mrr > 0.07
+
+
+def test_pointwise_synthetic_hybrid():
+
+    interactions = synthetic.generate_content_based(random_state=RANDOM_STATE)
+
+    train, test = random_train_test_split(interactions,
+                                          random_state=RANDOM_STATE)
+
+    model = ImplicitFactorizationModel(loss='bpr',
+                                       n_iter=10,
+                                       batch_size=1024,
+                                       learning_rate=1e-2,
+                                       l2=0.0)
+
+    model.fit(train, verbose=True)
+    print(model._net)
+
+    # test.context_features *= 0
+    # test.user_features *= 0
+    # test.item_features *= 0
+
+    mrr = mrr_score(model, test, train=train).mean()
+
+    print('MRR {}'.format(mrr))
+
+    assert mrr > 0.05
