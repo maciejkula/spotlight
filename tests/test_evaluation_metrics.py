3077db7576b2af269e2637683a82ba6364e6a084
Nikki Six <nicolai.tesela@gmail.com>
2018-05-22 15:58:08 -0500

    TESTING: adding test for sequence_precision_recall evaluation metric

--- tests/test_evaluation_metrics.py
+++ tests/test_evaluation_metrics.py
@@ -4,7 +4,7 @@ import numpy as np
 
 import pytest
 
-from spotlight.evaluation import precision_recall_score
+from spotlight.evaluation import precision_recall_score, sequence_precision_recall_score
 from spotlight.cross_validation import random_train_test_split
 from spotlight.datasets import movielens
 from spotlight.factorization.implicit import ImplicitFactorizationModel
@@ -14,7 +14,7 @@ CUDA = bool(os.environ.get('SPOTLIGHT_CUDA', False))
 
 
 @pytest.fixture(scope='module')
-def data():
+def data_implicit_factorization():
 
     interactions = movielens.get_movielens_dataset('100K')
 
@@ -33,14 +33,57 @@ def data():
     return train, test, model
 
 
+@pytest.fixture(scope='module')
+def data_implicit_sequence():
+
+    interactions = movielens.get_movielens_dataset('100K')
+
+    train, test = random_train_test_split(interactions,
+                                          random_state=RANDOM_STATE)
+
+    h = hyperparameters
+
+    model = ImplicitSequenceModel(loss='adaptive_hinge',
+                                  representation='lstm',
+                                  batch_size=[8, 16, 32, 256],
+                                  learning_rate=[1e-3, 1e-2, 5 * 1e-2, 1e-1],
+                                  l2=[1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 0.0],
+                                  n_iter=list(range(1, 2)),
+                                  use_cuda=CUDA,
+                                  random_state=random_state)
+
+    model.fit(train, verbose=True)
+
+    return train, test, model
+
+
+@pytest.mark.parametrize('k', 10)
+def test_precision_recall(data_implicit_sequence, k):
+
+    (train, test, model) = data_implicit_sequence
+
+    interactions = movielens.get_movielens_dataset('100K')
+    train, test = random_train_test_split(interactions,
+                                          random_state=RANDOM_STATE)
+
+    precision, recall = sequence_precision_recall_score(model, test, train, k=k)
+
+    assert precision.shape == recall.shape
+
+    if not isinstance(k, list):
+        assert len(precision.shape) == 1
+    else:
+        assert precision.shape[1] == len(k)
+
+
 @pytest.mark.parametrize('k', [
     1,
     [1, 1],
     [1, 1, 1]
 ])
-def test_precision_recall(data, k):
+def test_precision_recall(data_implicit_factorization, k):
 
-    (train, test, model) = data
+    (train, test, model) = data_implicit_factorization
 
     interactions = movielens.get_movielens_dataset('100K')
     train, test = random_train_test_split(interactions,
